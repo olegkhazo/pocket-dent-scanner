af6e463ba37da3d581a6e18f7992fd1f29f0706c
Oleg Zhuravko <o.zhuravko@gmail.com>
2026-09-06 10:48:14 +0200

    fix(android): use plugins.withId instead of afterEvaluate for compileSdk override

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,11 +21,14 @@ subprojects {
 
 // Force all Flutter plugins to compile against SDK 36.
 subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
-            extensions.configure<com.android.build.gradle.BaseExtension> {
-                compileSdkVersion(36)
-            }
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 36
+        }
+    }
+    plugins.withId("com.android.application") {
+        extensions.configure<com.android.build.gradle.AppExtension> {
+            compileSdk = 36
         }
     }
 }
